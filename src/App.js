e76239641f9ffb8de4dfd810da45abfe984d6d9c
ElyesBenCheikh99 <elyes.ben-cheikh.1@ens.etsmtl.ca>
2025-12-16 20:25:11 +0900

    ll

--- src/App.js
+++ src/App.js
@@ -2,21 +2,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import LandingPage from './unAuth/LandingPage';
-import './App.css';
-import './firebase';
-
-function App() {
-  return (
-    <div className="app-structure-container">
-      <Helmet>
-        <meta
-          name="description"
-          content="TubeStamp is a free online tool that links for YouTube videos. Share specific moments from videos with your friends and audience."
-        />
-        <meta
-          name="keywords"
-          content="YouTube, timestamp, links, video, sharing, free, online, tool"
-        />
+imp
       </Helmet>
       <LandingPage />
     </div>
